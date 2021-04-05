12096e562df2f020655369dc65c4790d48140942
Marco Aurélio Bigélli Cardoso <macardoso95@gmail.com>
2021-04-05 09:57:28 -0300

    Swith to headless chrome crawler

--- test/discovery-web-crawler.test.js
+++ test/discovery-web-crawler.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 const DiscoveryV1 = require('ibm-watson/discovery/v1')
-const Crawler = require('simplecrawler')
 const DiscoveryWebCrawler = require('../lib')
 
 
@@ -8,23 +7,22 @@ let discoveryMock = {
     updateDocument: jest.fn(x => Promise.resolve(x))
 }
 
-let getSimpleCrawlerMock = () => ({
-    stopped: false,
-    stop: () => this.stopped = true,
-    start: async () => {
-        let urls = ['foo_url', 'foo_url/target_path', 'foo_url/target_path/subpath_1', 'foo_url/target_path/subpath_2', 'foo_url/other_path', 'foo_url/assets']
+let getCrawlerMock = (crawlerOptions) => ({
+    queue: async () => {
+        let urls = ['https://foo_url', 'https://foo_url/target_path', 'https://foo_url/target_path/subpath_1', 'https://foo_url/target_path/subpath_2', 'https://foo_url/other_path', 'https://foo_url/assets']
         for (let i = 0; i < urls.length; i++) {
-            // if (this.stopped) break
-            if (this.fetchCondition({ url: urls[i] }))
-                await this.fetchcomplete({ url: urls[i] }, Buffer.from(`<head><title>Foo title ${i}</title><meta name="foo" content="bar"></head><main>Foo content ${i}</main>`))
+            if (crawlerOptions.preRequest({ url: urls[i] }))
+                try { crawlerOptions.evaluatePage() } catch (err) {/** This will fail because it's run in a browser */ }
+            if (i == 0) await crawlerOptions.onError(`Some error happened with url ${urls[i]}`)
+            else await crawlerOptions.onSuccess({ response: { url: urls[i] }, result: { data: `<head><title>Foo title ${i}</title><meta name="foo" content="bar"></head><main>Foo content ${i}</main>` } })
         }
-        this.complete()
     },
-    on: (name, func) => this[name] = func,
-    addFetchCondition: (func) => this.fetchCondition = func
+    onIdle: () => new Promise(r => setTimeout(r, 500)),
+    close: () => new Promise(r => setTimeout(r, 500)),
 })
 
 let options1 = {
+    createCrawler: getCrawlerMock,
     serviceUrl: 'foo_service_url',
     version: 'foo_version',
     apikey: 'foo_apikey',
@@ -32,19 +30,20 @@ let options1 = {
     collectionId: 'foo_collection_id',
     maxPages: 3,
 
-    url: 'foo_url',
+    url: 'https://foo_url',
     dryRun: true,
     verbose: true,
 }
 
 let options2 = {
+    createCrawler: getCrawlerMock,
     serviceUrl: 'foo_service_url',
     version: 'foo_version',
     apikey: 'foo_apikey',
     environmentId: 'foo_environment_id',
     collectionId: 'foo_collection_id',
 
-    url: 'foo_url/target_path',
+    url: 'https://foo_url/target_path',
 
     parse: async $ => ({ text: $('main').text().replace(/\s+/g, ' ').trim() }),
     urlCondition: url => !url.includes('/subpath_2'),
@@ -62,7 +61,6 @@ describe('DiscoveryWebCrawler', () => {
             expect(crawler.discoveryV1.baseOptions.serviceUrl).toBe('foo_service_url')
             expect(crawler.discoveryV1.baseOptions.version).toBe('foo_version')
             expect(crawler.discoveryV1.authenticator.apikey).toBe('foo_apikey')
-            expect(crawler.simpleCrawler).toBeInstanceOf(Crawler)
         })
     })
 
@@ -71,7 +69,7 @@ describe('DiscoveryWebCrawler', () => {
             let crawler = new DiscoveryWebCrawler(options1)
             it('Executes crawling and logs documents', (done) => {
                 crawler.discoveryV1 = discoveryMock
-                crawler.simpleCrawler = getSimpleCrawlerMock()
+                crawler.simpleCrawler = getCrawlerMock()
                 crawler.start()
                     .catch(err => done.fail(err))
                     .then(() => {
@@ -84,7 +82,7 @@ describe('DiscoveryWebCrawler', () => {
             let crawler = new DiscoveryWebCrawler(options2)
             it('Executes crawling and indexes documents', (done) => {
                 crawler.discoveryV1 = discoveryMock
-                crawler.simpleCrawler = getSimpleCrawlerMock()
+                crawler.simpleCrawler = getCrawlerMock()
                 crawler.start()
                     .catch(err => done.fail(err))
                     .then(() => {
@@ -93,20 +91,20 @@ describe('DiscoveryWebCrawler', () => {
                         delete doc.file
                         delete file.crawler_date
                         expect(doc).toEqual({
-                            'documentId': '4d88fddafbbe0da02bf61b98e0cf39cb9a1cf32a',
+                            'documentId': '821d28a5c61ad6c383e06a8bcce86fc4fffb69b6',
                             'environmentId': 'foo_environment_id',
                             'collectionId': 'foo_collection_id',
                             'fileContentType': 'application/json',
                             'fileName': 'Foo title 2',
                         })
                         expect(file).toEqual({
-                            'crawler_url': 'foo_url/target_path',
+                            'crawler_url': 'https://foo_url/target_path',
                             'meta': {
                                 'foo': 'bar',
                             },
                             'text': 'Foo content 2',
                             'title': 'Foo title 2',
-                            'url': 'foo_url/target_path/subpath_1',
+                            'url': 'https://foo_url/target_path/subpath_1',
                         })
                         done()
                     })
